dd6fbd5e172830331f3bd6d176c9ad9405a37f0e
Remi3356 <114180103+Remi3356@users.noreply.github.com>
2022-10-05 23:27:02 +0300

    Add files via upload

---
+++ Even numbers using for loop.cpp
@@ -0,0 +1,17 @@
+// A program used to display even numbers using for loop
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{    
+     int i;
+     // the range for where the even numbers are supposed to reach
+     for(i=1; i<=10;i++)
+    if (i%2==0)
+    cout<<" "<<i;
+
+    return 0;
+
+}
