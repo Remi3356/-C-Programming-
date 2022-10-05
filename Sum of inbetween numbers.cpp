7d5192307965d69b2cbfe7130abd63f6dd11da7d
Remi3356 <114180103+Remi3356@users.noreply.github.com>
2022-10-05 19:53:11 +0300

    Add files via upload

---
+++ Sum of inbetween numbers.cpp
@@ -0,0 +1,22 @@
+//This is a program to calculate the sum of inbetween certain numbers of the users choice.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i,x,y,sum=0;
+   cout <<"enter the starting point"<<endl;
+   cin>>x;
+   cout<<"enter the ending point"<<endl;
+   cin>>y;
+   // Using the for loop for repetition control structure that allows us to write a loop that is executed a specific number of times. 
+   for(i=x;i<=y;i++)
+      {
+          cout<<i<<endl;
+          sum+=i;
+      }
+      cout<<"The sum of the numbers from "<<x<<" to "<<y<<" is "<<sum;
+      return 0;
+}
